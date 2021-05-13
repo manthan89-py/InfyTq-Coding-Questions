4bb91f034f9e4195252d9e028a611f2639124385
Manthan Bhikadiya ♚ <61309101+manthan89-py@users.noreply.github.com>
2021-05-13 13:35:27 +0530

    Update question 9.py

--- Python/question 9.py
+++ Python/question 9.py
@@ -1,3 +1,17 @@
+# We are given a list and number of elements deletion X.
+# We have to delete X elements from the list so that at the end we get minimum unique elemetns in the list.
+# Print the final minimum no of unique elements in the list
+
+# Test Case
+# 4
+# 1 1 1 2 2 3 3 4 5 6
+# output : 3
+
+# Test Case
+# 2
+# 1 1 2 3 4 4
+# output : 2
+
 
 target = int(input())
 list_num = list(map(int , input().split(" ")))
