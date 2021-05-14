6f7676c35f4c888c9d40702b709134aa7d8f2ca6
Manthan Bhikadiya ♚ <61309101+manthan89-py@users.noreply.github.com>
2021-05-14 10:24:07 +0530

    Update question 16.py

--- Python/question 16.py
+++ Python/question 16.py
@@ -1,3 +1,11 @@
+# Consider MxN matrix m1 and PxQ matrix M2 cosisting of positive integers. Create and print outmatrix based ont that below logic.
+# For every number num1 from M1 at row r1 and column c1 :
+# (1) Multiply with each of the numbers num2 of M2 such that num2 is in (r1,r1) , (r1,c1) , (c1,c1) , (c1,r1) if exists.
+# (2) Identify and add the product that occurs maximum number of times to outmatirx at row r1 and column c1.
+# (3) If there are multiple product with maximum occurence then minimum value will be added yo row r1 anc column c1.
+# (4) if both row and column in M2 doesn't exist equal to r1 and c1 then add -1 in the outmatrix at row r1 and column c1.
+
+
 from collections import Counter
 try:
     m = int(input())
