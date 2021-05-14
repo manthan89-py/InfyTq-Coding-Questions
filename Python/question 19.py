4b341622cb7f493f04f818bd99f43daa14408f40
Manthan Bhikadiya ♚ <61309101+manthan89-py@users.noreply.github.com>
2021-05-14 10:35:46 +0530

    Update question 19.py

--- Python/question 19.py
+++ Python/question 19.py
@@ -1,3 +1,10 @@
+# We are given one string.
+# If string contain vowel then multiply its index with 5 and store it in result variable.
+# Add all even number starting from 1 to result variable. 
+# If adition of all even number is in more then two digits then we have to to addition of digits untill answer will not be in single digit.
+# After getting the final answer replace vowel with that particular number.
+# Print the output string.
+
 instr = input()
 outstr = ''
 vowels = ['A','E','I','O','U','a','e','i','o','u']
